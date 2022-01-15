8e0a76546e3e0f7d615ec5134171d9985593ec63
Nikolai Merritt <nikolaimerritt20@gmail.com>
2022-01-15 18:53:16 +0000

    Added Cat (test class) and DatabaseView

---
+++ Assets/Backend/Cat.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Neo4JHelloWorld;
+
+public class Cat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Test: printing out ID of each node in database
+        using var database = new DatabaseView("bolt://localhost:7687", "neo4j", "password");
+        foreach (int id in database.AllNodeIds())
+        {
+            Debug.Log($"found node with index = {id}");
+        }
+
+        // var pageRanker = new PageRanker();
+        // foreach (int idx in pageRanker.AllNodeIndexes())
+        //     Debug.Log($"found node with index = {idx}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Backend/DatabaseView.cs
@@ -0,0 +1,68 @@
+﻿using Neo4j.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Neo4JHelloWorld
+{
+    public class DatabaseView : IDisposable
+    {
+        private bool _disposed = false;
+        private readonly IDriver _driver;
+
+        ~DatabaseView()
+            => Dispose(false);
+
+        public DatabaseView(string uri, string username, string password)
+            => _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(username, password));
+
+        /// <summary> Closes the database connection. </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+                _driver?.Dispose();
+
+            _disposed = true;
+        }
+
+        /// <summary> IDs of the nodes that <paramref name="nodeId"/> links to. </summary>
+        public List<int> NodeIdsLinkedFrom(int nodeId)
+            => NodeIdsMatchingQuery($"match (linkingNode) --> (baseNode) where id(linkingNode) = {nodeId} return id(baseNode)");
+
+        /// <summary> IDs of the nodes that link to <paramref name="nodeId"/>. </summary>
+        public List<int> NodeIdsLinkingTo(int nodeId)
+            => NodeIdsMatchingQuery($"match (linkingNode) --> (baseNode) where id(baseNode) = {nodeId} return id(linkingNode)");
+
+        /// <summary> The IDs of every node in the database. </summary>
+        public List<int> AllNodeIds()
+            => NodeIdsMatchingQuery("match (x) return id(x)");
+
+        /// <summary> The IDs returned by <paramref name="query"/>. <paramref name="query"/> must return node IDs. </summary>
+        private List<int> NodeIdsMatchingQuery(string query)
+        {
+            using var session = _driver.Session();
+
+            List<int> nodeIds = session.ReadTransaction(tx =>
+            {
+                var result = tx.Run(query);
+                List<int> ids = new();
+
+                foreach (var record in result)
+                    ids.Add(record[0].As<int>());
+
+                return ids;
+            }).ToList();
+
+            return nodeIds;
+        }
+    }
+}
