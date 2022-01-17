7f9ef9b720ef9af49e3404b8c5626dbfbf60717b
joshuamills98 <57185163+joshuamills98@users.noreply.github.com>
2022-01-17 18:18:19 +0000

    Adding tree creation

--- Assets/Backend/NodeTree.cs
+++ Assets/Backend/NodeTree.cs
@@ -1,47 +1,62 @@
 using System;
 using System.Collections.Generic;
-
-class NodeTree
+namespace Backend
 {
-
-    // Node id
-    public Guid Id { get; private set; } = Guid.NewGuid();
-
-
-    // address of all children nodes 
-    public List<NodeTree> ChildNodes { get; private set; } = new List<NodeTree>();
-
-    // coordinates of the node
-    public (double x, double y, double z) Coordinates;
-
-    // node content
-    public string Content;
-
-
-    // methods
-    public void AddChildNode(NodeTree childNode)
+    class NodeTree
     {
-        ChildNodes.Add(childNode);
-    }
 
-    public void RemoveChildNode(NodeTree childNode)
-    {
-        if (ChildNodes.Remove(childNode))
+        public NodeTree((double x, double y, double z) coordinates, string content)
         {
-
+            Coordinates = coordinates;
+            Content = content;
+            Id = Guid.NewGuid();
         }
 
-        // TODO maybe return error?
-    }
-
-    public bool IsLeafNode()
-    {
-        if (this.ChildNodes.Count == 0)
+        public NodeTree((double x, double y, double z) coordinates, string content, Guid id)
         {
-            return true;
+            Coordinates = coordinates;
+            Content = content;
+            Id = id;
         }
-        return false;
-    }
+
+        // Node id
+        public Guid Id { get; private set; }
 
 
+        // address of all children nodes 
+        public List<NodeTree> ChildNodes { get; private set; } = new List<NodeTree>();
+
+        // coordinates of the node
+        public (double x, double y, double z) Coordinates;
+
+        // node content
+        public string Content;
+
+
+        // methods
+        public void AddChildNode(NodeTree childNode)
+        {
+            ChildNodes.Add(childNode);
+        }
+
+        public void RemoveChildNode(NodeTree childNode)
+        {
+            if (ChildNodes.Remove(childNode))
+            {
+
+            }
+
+            // TODO maybe return error?
+        }
+
+        public bool IsLeafNode()
+        {
+            if (this.ChildNodes.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }   
 }
