b7e57316cd04bab9ad30934ccfe57f6dfe5ad98f
Lichborne <frei.balazs@gmail.com>
2022-01-20 18:03:24 +0000

    NuGet

--- Assets/Backend/Cat.cs
+++ Assets/Backend/Cat.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Neo4JHelloWorld;
@@ -26,4 +26,4 @@ public class Cat : MonoBehaviour
     {
 
     }
-}
+}*/
