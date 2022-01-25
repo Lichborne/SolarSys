bd98c13cbb3bdc040d7e4ec2449aa8882fb93f4f
Lichborne <frei.balazs@gmail.com>
2022-01-25 16:29:50 +0000

    Update Drag.cs

--- Assets/ProjectScripts/Drag.cs
+++ Assets/ProjectScripts/Drag.cs
@@ -13,7 +13,7 @@ class Drag : MonoBehaviour
     private bool dragging = false;
     private float distance;
 
-    private float radius = 16;
+    private float radius = 36;
  
    
     void OnMouseEnter()
@@ -42,11 +42,15 @@ class Drag : MonoBehaviour
 
         int tries = 0;
 
+        /* better: find furthest based on average distance
+        List<Vector3> randomPoints = new List<Vector3>();
+        List<float> averageDistances = new List<float>();*/
+
         while (tries < 100) {
-            NewPosition = new Vector3(gameObject.transform.position.x+Random.Range(-radius/4, radius/4), gameObject.transform.position.y+Random.Range(-radius/4, radius/4), gameObject.transform.position.z+Random.Range(-radius/4, radius/4));
+            NewPosition = new Vector3(gameObject.transform.position.x+Random.Range(-radius/6, radius/6), gameObject.transform.position.y+Random.Range(-radius/6, radius/6), gameObject.transform.position.z+Random.Range(-radius/6, radius/6));
 
             foreach (Vector3 hitPoint in hitPoints) {
-                if (Vector3.Distance(NewPosition, hitPoint) < radius/4) {
+                if (Vector3.Distance(NewPosition, hitPoint) < radius/6) {
                     tries +=1;
                     continue;
                 }  
