a926cc17799286c42cd881bf832c04398a3a7e9e
sy3111 <sy3111@ic.ac.uk>
2022-02-18 09:01:26 +0000

    made project panel scrollable

--- Assets/ProjectScripts/SavedGraphs.cs
+++ Assets/ProjectScripts/SavedGraphs.cs
@@ -8,15 +8,13 @@ using Backend;
 
 public class SavedGraphs : MonoBehaviour
 {
-    //list of saved graphs by a user
-    // private List<GameObject> savedGraphsList;
+
     private List<string> projectList;
     private List<string> pathViewList;
     public Transform savedProjectsPanel;
     public Transform savedPathViewsPanel;
     public Transform savedProjectContainer;
     public Transform savedPathContainer;
-    public TextMeshProUGUI projectText;
     public TextMeshProUGUI pathViewText;
     public GameObject newProjectName;
     public GameObject createNewProjectPanel;
@@ -33,22 +31,31 @@ public class SavedGraphs : MonoBehaviour
         //for now put this in Start(), just to test
 
         // Commenting out Olivia's code
-        // string project1 = "Project1";
-        // string project2 = "Project2";
-        // string project3 = "Project3";
-        // string project4 = "Project4";
-        // string project5 = "Project5";
-        // string project6 = "Project6";
-        // string project7 = "Project7";
-        // AddProject(project1);
-        // AddProject(project2);
-        // AddProject(project3);
-        // AddProject(project4);
-        // AddProject(project5);
-        // AddProject(project6);
-        // AddProject(project7);
-        // Debug.Log(projectList.Count);
-        // RefreshSavedGraphs(projectList, savedProjectContainer, savedProjectsPanel, projectText); 
+        string project1 = "Project1";
+        string project2 = "Project2";
+        string project3 = "Project3";
+        string project4 = "Project4";
+        string project5 = "Project5";
+        string project6 = "Project6";
+        string project7 = "Project7";
+        string project8 = "Project8";
+        string project9 = "Project9";
+        string project10 = "Project10";
+        string project11 = "Project11";
+        string project12 = "Project12";
+
+        AddProject(project1, savedProjectContainer, savedProjectsPanel);
+        AddProject(project2, savedProjectContainer, savedProjectsPanel);
+        AddProject(project3, savedProjectContainer, savedProjectsPanel);
+        AddProject(project4, savedProjectContainer, savedProjectsPanel);
+        AddProject(project5, savedProjectContainer, savedProjectsPanel);
+        AddProject(project6, savedProjectContainer, savedProjectsPanel);
+        AddProject(project7, savedProjectContainer, savedProjectsPanel);
+        AddProject(project8, savedProjectContainer, savedProjectsPanel);
+        AddProject(project9, savedProjectContainer, savedProjectsPanel);
+        AddProject(project10, savedProjectContainer, savedProjectsPanel);
+        AddProject(project11, savedProjectContainer, savedProjectsPanel);
+        AddProject(project12, savedProjectContainer, savedProjectsPanel);
 
         //  //for now put this in contructor, just to test
         // string pathView1 = "PathView1";
@@ -68,74 +75,41 @@ public class SavedGraphs : MonoBehaviour
     }
 
     //function to append to list of graphs
-    public void AddProject(string newProject) 
+    public void AddProject(string newProjectName, Transform chosenContainer, Transform ChosenParentPanel) 
     {
-        projectList.Add(newProject);
-        RefreshSavedGraphs(projectList, savedProjectContainer, savedProjectsPanel, projectText);
+        projectList.Add(newProjectName);
+        RectTransform savedGraphRectTransform = Instantiate(chosenContainer, ChosenParentPanel).GetComponent<RectTransform>();
+        TextMeshProUGUI textComponent = savedGraphRectTransform.GetComponentInChildren<TextMeshProUGUI>();
+        textComponent.text = newProjectName;
+        Debug.Log(newProjectName);
+        savedGraphRectTransform.gameObject.SetActive(true);
+      
     }
 
     private void DisplayProjectTitles(List<string> projectTitles)
     {
         // someone pls remove these debug.logs
         Debug.Log($"Writing {projectTitles.Count} project titles");
-        foreach (string title in projectTitles)
+        foreach (string title in projectTitles) 
+        {
             Debug.Log($"Writing project title {title}");
+            AddProject(title, savedProjectContainer, savedProjectsPanel);
+        }
         
-        RefreshSavedGraphs(projectTitles, savedProjectContainer, savedProjectsPanel, projectText); 
     }
 
     //function to append to list of graphs
     public void AddPathView(string newPathView) 
     {
         pathViewList.Add(newPathView);
-        RefreshSavedGraphs(pathViewList, savedPathContainer, savedPathViewsPanel, pathViewText);
     }
 
-    // //function to get list of graphs, used by UISavedGraphs.cs
-    // public List<GameObject> GetSavedGraphsList() 
-    // {
-    //     return savedGraphsList;
-    // }
 
-    //  void Update()
-    // {   
-    //     //On left click
-    //     if (Input.GetMouseButtonDown(0))
-    //     {
-    //         Debug.Log(projectList.Count);
-    //     }
-    // }
-
-    private void RefreshSavedGraphs(List<string> chosenList, Transform chosenContainer, Transform ChosenParentPanel, TextMeshProUGUI chosenTextDisplay) 
-    {   
-        float x_displacement = 140f;
-        float y_displacement = -130f;
-        int x = 0;
-        int y = 0;
-        float savedGraphCellSize = 130f;
-        for (int index = 0; index < chosenList.Count; index++) 
-        {
-            RectTransform savedGraphRectTransform = Instantiate(chosenContainer, ChosenParentPanel).GetComponent<RectTransform>();
-            TextMeshProUGUI textComponent = savedGraphRectTransform.GetComponentInChildren<TextMeshProUGUI>();
-            textComponent.text = chosenList[index];
-            
-            savedGraphRectTransform.gameObject.SetActive(true);
-            savedGraphRectTransform.anchoredPosition = new Vector3(x_displacement + x * savedGraphCellSize, y_displacement -y* savedGraphCellSize, 0);
-
-            x++;
-            if(x > 4)
-            {
-                x = 0;
-                y++;
-            }
-        }
-    }
 
     //This function is called when we select create new project in pop-up panel
     public void CreateProjectButtonClicked()
     {   
         createNewProjectPanel.SetActive(false);
-        AddProject(newProjectName.GetComponent<TMP_InputField>().text);
-        RefreshSavedGraphs(projectList, savedProjectContainer, savedProjectsPanel, projectText);
+        AddProject(newProjectName.GetComponent<TMP_InputField>().text, savedProjectContainer, savedProjectsPanel);
     }
 }
