b74fee629232ed14e6d4b9504bc370b802ae1d5a
rouenlee29 <rowen.rouen.lee@gmail.com>
2022-02-19 11:11:57 +0000

    Add constructor for GraphProject using coroutine (commented out for the time being)

--- Assets/Backend/GraphProject.cs
+++ Assets/Backend/GraphProject.cs
@@ -22,6 +22,27 @@ namespace Backend
                 edge.Parent.Edges.Add(edge);
         }
 
+        // constructor using coroutines
+        // public GraphProject(string userEmail = "foo.bar@doc.ic.ac.uk", string projectTitle = "Test Project", string dbUri = /* "bolt://localhost:7687" */ "neo4j://cloud-vm-42-36.doc.ic.ac.uk:7687", string dbUsername = "neo4j", string dbPassword = "s3cr3t")
+        // {
+        //     ProjectId = (userEmail, projectTitle);
+        //     Database = new DatabaseView(dbUri, dbUsername, dbPassword);
+
+        //     yield return Database.ReadNodesFromProjectCo(this, processNodes);
+        //     yield return Database.ReadAllEdgesFromProjectCo(this, Nodes, processEdges);
+
+        // }
+
+        private void processNodes(List<GraphNode> nodes)
+        {
+            Nodes = nodes;
+        }
+
+        private void processEdges(List<GraphEdge> edges)
+        {
+            Edges = edges;
+        }
+
         public void Dispose()
             => Database.Dispose();
 
