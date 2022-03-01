45e51fd5dfb6100719b8d9d6438557e0fb5fbe2e
rouenlee29 <rowen.rouen.lee@gmail.com>
2022-03-01 16:55:25 +0000

    Create log nodes with validated json structure as body

--- Assets/Backend/LogNode.cs
+++ Assets/Backend/LogNode.cs
@@ -3,61 +3,130 @@ using System.Linq;
 using System.Collections.Generic;
 using static Backend.StringExtensions;
 
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
 using Newtonsoft.Json.Linq;
 
 namespace Backend
 {
     public enum ChangeEnum
     {
-        Create,
-        Update,
-        Delete
+        AddNode,
+        AddEdge,
+        UpdateNode,
+        UpdateEdge,
+        DeleteNode,
+        DeleteEdge
     }
     public class LogNode
     {
-        public ChangeEnum Change { get; private set; }
         public string Body { get; private set; }
-
+        public ChangeEnum Change { get; private set; }
         public Guid Id { get; private set; }
-
         public DateTime TimeStamp { get; private set; }
 
-        // public LogNode NextNode { get; private set; }
 
-        public LogNode(ChangeEnum change, string body)
+        public LogNode(ChangeEnum change)
         {
-            Id = Guid.NewGuid();
             Change = change;
-            Body = body;
+            Id = Guid.NewGuid();
             TimeStamp = DateTime.UtcNow;
         }
 
-        public LogNode(ChangeEnum change, string body, Guid id, DateTime timeStamp)
+        // Old code , to delete
+        public LogNode(ChangeEnum change, string body)
         {
-            Id = id;
             Change = change;
             Body = body;
+        }
+
+        public LogNode(string body, ChangeEnum change, Guid id, DateTime timeStamp)
+        {
+            Body = body;
+            Change = change;
+            Id = id;
             TimeStamp = timeStamp;
         }
 
-        // public static LogNode FromINode(INode dbNode)
-        // {
-        //     Enum.TryParse(dbNode.Properties["change"].As<string>(), out ChangeEnum change);
-        //     string body = dbNode.Properties["body"].As<string>();
-        //     Guid guid = Guid.Parse(dbNode.Properties["guid"].As<string>());
-        //     DateTime timeStamp = DateTime.Parse(dbNode.Properties["timestamp"].As<string>());
-
-        //     return new LogNode(change, body, guid, timeStamp);
-        // }
-
         public static LogNode FromJObject(GraphProject project, JObject obj)
         {
-            Enum.TryParse((string) obj["change"], out ChangeEnum change);
-            string body = (string) obj["body"];
-            Guid guid = Guid.Parse((string) obj["guid"]);
-            DateTime timeStamp = DateTime.Parse((string) obj["timestamp"]);
+            Enum.TryParse((string)obj["change"], out ChangeEnum change);
+            string body = (string)obj["body"];
+            Guid guid = Guid.Parse((string)obj["guid"]);
+            DateTime timeStamp = DateTime.Parse((string)obj["timestamp"]);
 
-            return new LogNode(change, body, guid, timeStamp);
+            return new LogNode(body, change, guid, timeStamp);
+        }
+
+        public static void Validate<T>(T obj)
+        {
+            var results = new List<ValidationResult>();
+
+            var validate = Validator.TryValidateObject(obj, new ValidationContext(obj), results, true);
+
+            if (!validate)
+            {
+                Console.WriteLine(String.Join("\n", results.Select(o => o.ErrorMessage)));
+                throw new InvalidDataException();
+            }
+        }
+
+        public class NodeCreationLog : LogNode
+        {
+            public NodeCreationLog(NodeCreationSchema log) : base(ChangeEnum.AddNode)
+            {
+                Validate(log);
+                Body = JsonSerializer.Serialize(log);
+            }
+        }
+
+        public class EdgeCreationLog : LogNode
+        {
+            public EdgeCreationLog(EdgeCreationSchema log) : base(ChangeEnum.AddEdge)
+            {
+                Validate(log);
+                Body = JsonSerializer.Serialize(log);
+            }
+        }
+
+        public class NodeUpdateLog : LogNode
+        {
+            public NodeUpdateLog(NodeUpdateSchema log) : base(ChangeEnum.UpdateNode)
+            {
+                Validate(log);
+                var options = new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
+                Body = JsonSerializer.Serialize(log, options);
+            }
+        }
+
+        public class EdgeUpdateLog : LogNode
+        {
+            public EdgeUpdateLog(EdgeUpdateSchema log) : base(ChangeEnum.UpdateEdge)
+            {
+                Validate(log);
+                var options = new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
+                Body = JsonSerializer.Serialize(log, options);
+            }
+        }
+
+        public class NodeDeletionLog : LogNode
+        {
+            public NodeDeletionLog(NodeCreationSchema log) : base(ChangeEnum.DeleteNode)
+            {
+                Validate(log);
+                Body = JsonSerializer.Serialize(log);
+            }
+        }
+
+        public class EdgeDeletionLog : LogNode
+        {
+            public EdgeDeletionLog(EdgeCreationSchema log) : base(ChangeEnum.DeleteEdge)
+            {
+                Validate(log);
+                Body = JsonSerializer.Serialize(log);
+            }
         }
     }
 }
---
+++ Assets/Backend/Schema.cs
@@ -0,0 +1,78 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend
+{
+    public class NodeCreationSchema
+    {
+        [Required]
+        string Title;
+
+        [Required]
+        string Body;
+
+        [Required]
+        Guid Id;
+
+        [Required]
+        (double X, double Y, double Z) Coordinates;
+    }
+
+    public class ValuePair<T>
+    {
+        [Required]
+        T NewValue;
+
+        [Required]
+        T OldValue;
+    }
+
+    public class NodeUpdateSchema
+    {
+        ValuePair<String> Title;
+
+        ValuePair<String> Body;
+
+        [Required]
+        Guid Id;
+
+        ValuePair<(double X, double Y, double Z)> Coordinates;
+    }
+
+    public class EdgeCreationSchema
+    {
+        [Required]
+        string Title;
+
+        [Required]
+        string Body;
+
+        [Required]
+        Guid Id;
+
+        [Required]
+        Guid ParentId;
+
+        [Required]
+        Guid ChildId;
+    }
+
+    public class EdgeUpdateSchema
+    {
+        [Required]
+        Guid Id;
+
+        ValuePair<string> Title;
+
+        ValuePair<string> Body;
+
+        ValuePair<Guid> ParentId;
+
+        ValuePair<Guid> ChildId;
+    }
+
+    public class NodeDeletionSchema
+    {
+
+
+    }
+}
